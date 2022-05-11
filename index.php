c620a5a3d5d469218d82d2f925eeca09af3535a0
stuhlmeister <63017374+dan-pri@users.noreply.github.com>
2022-05-11 14:23:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html lang="DE">
   <head>
     <meta charset="UTF-8">
@@ -6,8 +5,8 @@
   </head>
   <body>
 
-  <h1><a href="index.php">Admin Center Login</a></h1>
-  <form action="index.php" method="get">
+  <h1><a href="index.php">Admin Center - Login</a></h1>
+  <form action="index.php" method="post">
     <label for="iud">Username</label>
     <input type="text" name="username" id="iud">
 
@@ -16,10 +15,14 @@
     <input type="submit" value="Login">
   </form>
 <?
-if(isset($_GET['username']) && isset($_GET['password'])){
+if(isset($_POST['username']) && isset($_POST['password'])){
 
-  if(($_GET['username'] == "admin") && ($_GET['password'] == "H5o_ctF!ftw")){
-    echo("flag_5ecc8fd983a0ffa361dc1a55aab5120bc4449fd5_");
+  if(($_POST['username'] == "admin") && ($_POST['password'] == "H5o_ctF!ftw")){
+      echo("Welcome !<br><br>");
+      echo("flag_5ecc8fd983a0ffa361dc1a55aab5120bc4449fd5_");
+  }
+  else{
+    echo("Username or Passwort incorrect");
   }
 }
 ?>
